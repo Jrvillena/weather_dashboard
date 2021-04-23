0cdba827af9308264a6af69fc6baee5d5d00420a
justin <justinrvillena@gmail.com>
2021-04-22 18:34:34 -0700

    submit handler and save search function created

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
